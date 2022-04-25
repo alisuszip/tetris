8e254401d1c5426e03ae5d5f478176729454dc47
aumann <55710259+aumann10@users.noreply.github.com>
2022-04-25 22:43:02 +0300

    Add files via upload

---
+++ src/main/java/com/example/tetrisnoactivity/constants/FieldConstants.kt
@@ -0,0 +1,9 @@
+package com.example.tetrisnoactivity.constants
+
+enum class FieldConstants(val value: Int) {
+    COLUMN_COUNT(10), ROW_COUNT(20);
+}
+
+enum class CellConstants(val value: Byte) {
+    EMPTY(0), EPHEMERAL(1)
+}
---
+++ src/main/java/com/example/tetrisnoactivity/helper/HelperFunctions.kt
@@ -0,0 +1,8 @@
+package com.example.tetrisnoactivity.helper
+
+fun array2dOfByte(sizeOuter: Int, sizeInner: Int): Array<ByteArray> = Array(sizeOuter) {
+    ByteArray(sizeInner)
+}
+class HelperFunctions {
+
+}
